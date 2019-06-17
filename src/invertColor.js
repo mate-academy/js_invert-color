1be32defa6e40436e1d9986a843eb4ceedabce0c
OleksandrHlybchenko <logista@meta.ua>
2019-06-18 00:00:00 +0300

    for review

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,20 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  const hex = '0123456789abcdef';
+  const hexInvert = [...hex].reverse();
+  const startColor = color.toLowerCase().split('');
+  startColor.shift();
+  const inverter = (elem) => {
+    let invertElem = '';
+    invertElem = hex.split('')[hexInvert.indexOf(elem)];
+    return invertElem;
+  };
+  const finishColor = startColor.map(inverter);
+  finishColor.unshift('#');
+  const result = finishColor.join('').toUpperCase();
+
+  return result;
 }
 
 module.exports = invertColor;
