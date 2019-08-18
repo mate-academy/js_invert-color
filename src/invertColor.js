991a3a61fb30616f360d0a2f63105865c7088baf
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-18 21:43:22 +0300

    Solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,41 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  let invertedColor = '#';
+  const colorArray = color.split('');
+
+  for (let i = 1; i < colorArray.length; i++) {
+    invertedColor += invert(colorArray[i]);
+  }
+
+  return invertedColor;
+}
+
+function invert(colorPart) {
+  const hexMapping = new Map();
+  hexMapping.set(10, 'A');
+  hexMapping.set(11, 'B');
+  hexMapping.set(12, 'C');
+  hexMapping.set(13, 'D');
+  hexMapping.set(14, 'E');
+  hexMapping.set(15, 'F');
+  hexMapping.set('F', 15);
+  hexMapping.set('E', 14);
+  hexMapping.set('D', 13);
+  hexMapping.set('C', 12);
+  hexMapping.set('B', 11);
+  hexMapping.set('A', 10);
+
+  let invertedValue;
+  const hexMaxNumber = 15;
+
+  if (!isNaN(parseInt(colorPart))) {
+    invertedValue = hexMaxNumber - colorPart < 10
+      ? hexMaxNumber - colorPart : hexMapping.get(hexMaxNumber - colorPart);
+  } else {
+    invertedValue = hexMaxNumber - hexMapping.get(colorPart.toUpperCase());
+  }
+  return invertedValue;
 }
 
 module.exports = invertColor;
