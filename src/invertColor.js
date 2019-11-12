d471574812917cac7366de114abe8b55fc27d5af
Anna Gerdii <annagerdiy@gmail.com>
2019-11-12 14:37:17 +0200

    solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,17 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  const hex
+    = ['0', '1', '2', '3', '4', '5', '6', '7',
+      '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  const colorNormalized = color.toLowerCase().slice(1);
+  const colorInverted = [];
+
+  for (let i = 0; i < colorNormalized.length; i++) {
+    colorInverted.push(hex[hex.length - 1 - hex.indexOf(colorNormalized[i])]);
+  }
+
+  return '#' + colorInverted.join('');
 }
 
 module.exports = invertColor;
