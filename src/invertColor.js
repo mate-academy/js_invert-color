c2a4d68a1af7e2012ed56d84261d09e0a71c35e6
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-01-20 17:26:28 +0200

    solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -14,6 +14,16 @@
  */
 function invertColor(color) {
   // write code here
+
+  const HEX = '0123456789abcdef';
+  const lenghtHEX = 15;
+  const arr = color.split('').reverse();
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = HEX[lenghtHEX - HEX.indexOf(arr[i].toLowerCase())];
+  }
+
+  return arr.reverse().join('');
 }
 
 module.exports = invertColor;
