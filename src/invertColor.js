af81646efbf533b8564630dc35ac314adce3b656
masha <mariafil2107@gmail.com>
2020-01-21 16:24:06 +0200

    invertedColor

--- src/invertColor.js
+++ src/invertColor.js
@@ -14,6 +14,17 @@
  */
 function invertColor(color) {
   // write code here
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let invertedColor = '#';
+
+  color.toUpperCase().replace('#', '').split('').forEach(i => {
+    const index = numbers.indexOf(i);
+
+    invertedColor += numbers.reverse()[index];
+  });
+
+  return invertedColor;
 }
 
 module.exports = invertColor;
