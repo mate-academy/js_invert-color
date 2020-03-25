e0716c0eb7677110cfd08ce57c83df934b759a69
Andrey Faychak <faychak333@gmail.com>
2020-03-25 17:14:47 +0200

    'solution'

--- src/invertColor.js
+++ src/invertColor.js
@@ -14,6 +14,48 @@
  */
 function invertColor(color) {
   // write code here
+  const invert = (number) => {
+    switch (number) {
+      case "F" :
+        return 15;
+      case "E" :
+        return 14;
+      case "D" :
+        return 13;
+      case "C" :
+        return 12;
+      case "B" :
+        return 11;
+      case "A" :
+        return 10;
+      default:
+        return number;
+    }
+  }
+  const parse = (number) => {
+    switch (number) {
+      case 15 :
+        return "F";
+      case 14 :
+        return "E";
+      case 13 :
+        return "D";
+      case 12 :
+        return "C";
+      case 11 :
+        return "B";
+      case 10 :
+        return "A";
+      default:
+        return number;
+    }
+  }
+  let invertColor = color.slice(1)
+    .toUpperCase()
+    .split("")
+    .map(char => parse(15 - invert(char)))
+    .join("");
+  return "#" + invertColor;
 }
 
 module.exports = invertColor;
