fb145f697172525ca1af7d592816688d022d7c26
Vitalii Shytikov <v.shytikov@gmail.com>
2020-03-26 18:32:58 +0200

    solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,32 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  const noHashColor = color.slice(1).toUpperCase();
+  const white = parseInt(0xFF);
+  let red = (white - parseInt('0x' + noHashColor[0] + noHashColor[1]))
+    .toString(16);
+
+  if (red.length === 1) {
+    red = '0' + red;
+  }
+
+  let green = (white - parseInt('0x' + noHashColor[2] + noHashColor[3]))
+    .toString(16);
+
+  if (green.length === 1) {
+    green = '0' + green;
+  }
+
+  let blue = (white - parseInt('0x' + noHashColor[4] + noHashColor[5]))
+    .toString(16);
+
+  if (blue.length === 1) {
+    blue = '0' + blue;
+  }
+
+  const invertedColor = '#' + red + green + blue;
+
+  return invertedColor;
 }
 
 module.exports = invertColor;
