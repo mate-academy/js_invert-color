6aa67242451da75e12b29ccb6d7a4897a0be5d6e
Illia Oliynik <illiaoliynik@gmail.com>
2020-03-29 21:21:27 +0300

    solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,21 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  // eslint-disable-next-line no-param-reassign
+  color = color.toUpperCase().slice(1).split('');
+
+  // eslint-disable-next-line no-shadow
+  let invertColor = '#';
+  const systemTo16 = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+  const invertSystem = systemTo16.concat().reverse();
+
+  for (let i = 0; i < color.length; i++) {
+    invertColor += invertSystem[systemTo16.indexOf(color[i])];
+  }
+
+  return invertColor;
 }
 
 module.exports = invertColor;
