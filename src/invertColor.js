5da04ce2708842ab1eb1b00323fc7be18c491832
Denys <gryshko_denys@gmail.com>
2020-05-23 11:47:17 +0300

    solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -13,7 +13,40 @@
  * @return {string}
  */
 function invertColor(color) {
-  // write code here
+  const hexNumberR = color.slice(1, 3);
+  const hexNumberG = color.slice(3, 5);
+  const hexNumberB = color.slice(5);
+
+  const decimalNumberR = parseInt(hexNumberR, 16);
+  const decimalNumberG = parseInt(hexNumberG, 16);
+  const decimalNumberB = parseInt(hexNumberB, 16);
+
+  const whiteHexNumberR = 255;
+  const whiteHexNumberG = 255;
+  const whiteHexNumberB = 255;
+
+  const differenceR = whiteHexNumberR - decimalNumberR;
+  let differenceHexR = differenceR.toString(16);
+
+  if (differenceHexR.length === 1) {
+    differenceHexR = '0' + differenceHexR;
+  }
+
+  const differenceG = whiteHexNumberG - decimalNumberG;
+  let differenceHexG = differenceG.toString(16);
+
+  if (differenceHexG.length === 1) {
+    differenceHexG = '0' + differenceHexG;
+  }
+
+  const differenceB = whiteHexNumberB - decimalNumberB;
+  let differenceHexB = differenceB.toString(16);
+
+  if (differenceHexB.length === 1) {
+    differenceHexB = '0' + differenceHexB;
+  }
+
+  return `#${differenceHexR}${differenceHexG}${differenceHexB}`;
 }
 
 module.exports = invertColor;
