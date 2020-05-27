3003dfb846a453de72bff54391f37473cda22115
VlaDinter <vladyslavdinter@gmail.com>
2020-05-27 06:18:57 +0300

    Solution

--- src/invertColor.js
+++ src/invertColor.js
@@ -1,19 +1,18 @@
 'use strict';
 
-/**
- * Write a function accepting 6 digit HEX color with a leading #
- * and returning inverted color (color + inverted === white)
- *
- * invertColor('#000000') === '#FFFFFF' // 0x000000 + 0xFFFFFF === 0xFFFFFF
- * invertColor('#DDEEAA') === '#221155' // 0x221155 + 0xDDEEAA === 0xFFFFFF
- * invertColor('#012345') === '#FEDCBA' // 0x012345 + 0xFEDCBA === 0xFFFFFF
- *
- * @param {string} color
- *
- * @return {string}
- */
 function invertColor(color) {
-  // write code here
+  let invertHexColor = '';
+
+  for (const hexNumber of color) {
+    if (hexNumber === '#') {
+      invertHexColor += hexNumber;
+      continue;
+    }
+
+    invertHexColor += (0xF - ('0x' + hexNumber)).toString(16);
+  }
+
+  return invertHexColor;
 }
 
 module.exports = invertColor;
